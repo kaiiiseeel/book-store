82c7e4d43e24e29472d2dc550cb5f7f31d6a6de3
Mykola Skrypalov <157539529+kaiiiseeel@users.noreply.github.com>
2024-04-22 14:25:33 +0300

    added a "sketch" of repo, entity and service (#1)
    * added a "sketch" of repo, entity and service
    
    * checkstyle fixes
    
    * implemented logic to save and findAll() methods
    
    * removed "Lombok Data"

--- src/main/java/mate/academy/bookstore/BookStoreApplication.java
+++ src/main/java/mate/academy/bookstore/BookStoreApplication.java
@@ -1,7 +1,9 @@
 package mate.academy.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
@@ -10,4 +12,8 @@ public class BookStoreApplication {
         SpringApplication.run(BookStoreApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(BookStoreApplication application) {
+        return args -> {};
+    }
 }
