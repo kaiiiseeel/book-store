e95092eb3d202dda1d8396f003c1ac2bfd0dfbf9
Mykola Skrypalov <skripalov1@gmail.com>
2024-05-09 22:59:45 +0300

    added authentication controller

---
+++ src/main/java/mate/academy/bookstore/controller/AuthenticationController.java
@@ -0,0 +1,32 @@
+package mate.academy.bookstore.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import mate.academy.bookstore.dto.UserRegistrationRequestDto;
+import mate.academy.bookstore.dto.UserResponseDto;
+import mate.academy.bookstore.exception.RegistrationException;
+import mate.academy.bookstore.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "User authentication",
+        description = "Endpoints for registration and authentication users")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+    private final UserService userService;
+
+    @PostMapping("/registration")
+    @Operation(summary = "Register new user",
+            description = "Registering a new user, returns registered user," +
+                    " throws an exception if a user already exists")
+    public UserResponseDto register(
+            @RequestParam @Valid UserRegistrationRequestDto request) throws RegistrationException {
+        return userService.register(request);
+    }
+}
