6a056a11f2be9a2dc4ede5cb60e704575679c678
Mykola Skrypalov <skripalov1@gmail.com>
2024-05-13 22:33:06 +0300

    updates according to comments

--- src/main/java/mate/academy/bookstore/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/mate/academy/bookstore/exception/CustomGlobalExceptionHandler.java
@@ -28,24 +28,20 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
             HttpHeaders headers,
             HttpStatusCode status,
             WebRequest request) {
-        Map<String, Object> body = new LinkedHashMap<>();
         List<String> errors = ex.getBindingResult().getAllErrors().stream()
                 .map(this::getErrorMessage)
                 .toList();
-        body.put(TIMESTAMP, LocalDateTime.now());
-        body.put(STATUS, HttpStatus.BAD_REQUEST);
-        body.put(ERRORS, errors);
-        return new ResponseEntity<>(body, headers, status);
+        return handleException(HttpStatus.BAD_REQUEST, errors);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    protected ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException exception) {
+        return handleException(HttpStatus.NOT_FOUND, exception.getMessage());
     }
 
     @ExceptionHandler(RegistrationException.class)
-    public ResponseEntity<Object> handleRegistrationException(RegistrationException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
+    protected ResponseEntity<Object> handleRegistrationException(RegistrationException exception) {
+        return handleException(HttpStatus.CONFLICT, exception.getMessage());
     }
 
     private String getErrorMessage(ObjectError e) {
@@ -55,5 +51,13 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
             return field + " " + message;
         }
         return e.getDefaultMessage();
+    }
+
+    private ResponseEntity<Object> handleException(HttpStatus status, Object errors) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put(TIMESTAMP, LocalDateTime.now());
+        body.put(STATUS, HttpStatus.BAD_REQUEST);
+        body.put(ERRORS, errors);
+        return new ResponseEntity<>(body, status);
     }
 }
--- src/main/java/mate/academy/bookstore/validation/FieldMatchValidator.java
+++ src/main/java/mate/academy/bookstore/validation/FieldMatchValidator.java
@@ -2,8 +2,9 @@ package mate.academy.bookstore.validation;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+
+import java.lang.reflect.Field;
 import java.util.Objects;
-import org.springframework.beans.BeanWrapperImpl;
 
 public class FieldMatchValidator implements ConstraintValidator<FieldMatch,
         Object> {
@@ -19,9 +20,21 @@ public class FieldMatchValidator implements ConstraintValidator<FieldMatch,
     @Override
     public boolean isValid(Object value,
                            ConstraintValidatorContext constraintValidatorContext) {
-        Object firstObject = new BeanWrapperImpl(value).getPropertyValue(firstField);
-        Object secondObject = new BeanWrapperImpl(value).getPropertyValue(secondField);
+        try {
+            Object firstObject = getFieldValue(value, firstField);
+            Object secondObject = getFieldValue(value, secondField);
+            return Objects.equals(firstObject, secondObject);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            return false;
+        }
 
-        return Objects.equals(firstObject, secondObject);
+    }
+
+    private Object getFieldValue(Object value, String fieldName)
+            throws NoSuchFieldException, IllegalAccessException {
+        Class<?> clazz = value.getClass();
+        Field field = clazz.getDeclaredField(fieldName);
+        field.setAccessible(true);
+        return field.get(value);
     }
 }
