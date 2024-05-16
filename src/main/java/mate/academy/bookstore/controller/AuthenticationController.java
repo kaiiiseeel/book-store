3f39cf5c9f615aaec1f3cd816135ec07ffa20c72
Mykola Skrypalov <157539529+kaiiiseeel@users.noreply.github.com>
2024-05-16 19:11:57 +0300

    added JWT to the project (#8)
    * created User entity, User dtos, added validation for it
    
    * added user mapper
    
    * added registration exception
    
    * added user repository and service
    
    * added authentication controller
    
    * *checkstyle fixes*
    
    * added soft delete to user entity
    
    * updated FieldMatchValidator
    
    * added basic security configuration (UserDetailsService, Configuration class)
    
    * added role entity, added the necessary liquibase changesets and tables
    
    * added security and role-based access to the project
    
    * updates according to comments
    
    * *checkstyle fixes*
    
    * fix github builds
    
    * returned the necessary method
    
    * *checkstyle fixes*
    
    * added dto classes for login
    
    * added necessary dependencies
    
    * implemented JWT to the project
    
    * made a composite key
    
    * moved all sensitive properties to env variables
    
    * fixes

--- src/main/java/mate/academy/bookstore/controller/AuthenticationController.java
+++ src/main/java/mate/academy/bookstore/controller/AuthenticationController.java
@@ -4,13 +4,16 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import mate.academy.bookstore.dto.UserLoginRequestDto;
+import mate.academy.bookstore.dto.UserLoginResponseDto;
 import mate.academy.bookstore.dto.UserRegistrationRequestDto;
 import mate.academy.bookstore.dto.UserResponseDto;
 import mate.academy.bookstore.exception.RegistrationException;
+import mate.academy.bookstore.security.AuthenticationService;
 import mate.academy.bookstore.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "User authentication",
@@ -20,13 +23,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/registration")
     @Operation(summary = "Register new user",
             description = "Registering a new user, returns registered user,"
                     + " throws an exception if a user already exists")
     public UserResponseDto register(
-            @RequestParam @Valid UserRegistrationRequestDto request) throws RegistrationException {
+            @RequestBody @Valid UserRegistrationRequestDto request) throws RegistrationException {
         return userService.register(request);
+    }
+
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody UserLoginRequestDto requestDto) {
+        return authenticationService.authenticate(requestDto);
     }
 }
---
+++ src/main/java/mate/academy/bookstore/dto/UserLoginRequestDto.java
@@ -0,0 +1,16 @@
+package mate.academy.bookstore.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserLoginRequestDto(
+        @Email
+        String email,
+        @NotBlank(message = "Your password can't be empty")
+        @Size(min = 8, max = 20,
+                message = "Your password must contain at least 8 characters"
+                        + " and at most 20 characters")
+        String password
+) {
+}
